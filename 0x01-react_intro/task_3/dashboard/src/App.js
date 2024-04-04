3dc92aba1bfb8a6bc0740c5467dd56214f8d2c33
Adetule-ayomide <adetuleayomide@gmail.com>
2024-04-04 22:43:08 +0100

    react

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { getFullYear, getFooterCopy } from "./utils";
+import logo from "./Holberton_logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr />
+
+      <body className="App-body">
+        <p>Login to access the full dashboard:</p>
+        <form action="">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" />
+
+          <button>OK</button>
+        </form>
+      </body>
+      <hr />
+      <footer className="App-footer">
+        <p>{getFooterCopy(true)}</p>
+        <p>{getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
+export default App;
