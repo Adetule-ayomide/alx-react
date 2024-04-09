1002a7d1f10be6974857301c410f83ddcc1342ff
Adetule-ayomide <adetuleayomide@gmail.com>
2024-04-10 00:17:45 +0100

    react

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,26 +1,35 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
   },
-  devServer: {
-    contentBase: "./dist",
-    hot: true,
-  },
-  devtool: "inline-source-map",
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
     ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
+    open: true,
   },
 };
